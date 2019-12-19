d342e47652b2867486030a5e24e84082b921a93a
srl <906239+srliao@users.noreply.github.com>
2019-12-19 08:51:06 -0500

    fix sudo status re docker

--- .initial/setup.sh
+++ .initial/setup.sh
@@ -36,6 +36,7 @@ sudo add-apt-repository \
 sudo apt-get update
 sudo apt-get install docker-ce -y
 sudo usermod -aG docker ${USER}
+su - ${USER}
 
 #docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
